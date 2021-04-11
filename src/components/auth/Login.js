5e49ffb732aab9aabfcedafe622434f5a932cacc
saravanan-itilite <saravanan.cm@itilite.com>
2021-04-11 11:48:50 +0530

    login bg handled

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -12,6 +12,7 @@ import Button from "@material-ui/core/Button";
 import { Container, Card, CardContent } from "@material-ui/core";
 import Navbar from "../../components/layout/Navbar";
 import loveLeft from "../../assets/images/love.gif";
+import wed from "../../assets/images/wed.jpg";
 import loveRight from "../../assets/images/love-in-air.gif";
 
 class Login extends Component {
@@ -61,132 +62,134 @@ class Login extends Component {
 	render() {
 		const { errors } = this.state;
 		return (
-			<div className='container'>
-				<Navbar customProps={this.state} />
-				<div
-					style={{
-						marginTop: "3rem",
-						backgroundSize: "25%, 30%",
-						backgroundPosition: "5% 20%, right bottom",
-						backgroundImage:
-							"url(" + loveLeft + "), url(" + loveRight + ")",
-						backgroundRepeat: "no-repeat, no-repeat",
-					}}
-					className='row'>
-					<Container maxWidth='xs'>
-						<Card>
-							<CardContent>
-								<div
-									className='col s12'
-									style={{
-										display: "flex",
-										flexDirection: "column",
-										alignItems: "center",
-										marginBottom: "2rem",
-									}}>
-									<Avatar>
-										<LockOutlinedIcon />
-									</Avatar>
-									<Typography component='h1' variant='h5'>
-										Sign in
-									</Typography>
-								</div>
-								<form noValidate onSubmit={this.onSubmit}>
-									<div className='input-field col s12'>
-										<TextField
-											onChange={this.onChange}
-											value={this.state.email}
-											error={errors.email}
-											id='outlined-basic'
-											name='email'
-											type='email'
-											className={classnames("", {
-												invalid:
-													errors.email ||
-													errors.emailnotfound,
-											})}
-											style={{ width: "100%" }}
-											label='Email'
-											variant='outlined'
-										/>
-										<span className='red-text'>
-											{errors.email}
-											{errors.emailnotfound}
-										</span>
-									</div>
-									<div className='input-field col s12'>
-										<TextField
-											onChange={this.onChange}
-											value={this.state.password}
-											error={errors.password}
-											id='outlined-basic'
-											name='password'
-											type='password'
-											className={classnames("", {
-												invalid:
-													errors.password ||
-													errors.passwordincorrect,
-											})}
-											style={{
-												width: "100%",
-												marginTop: "0.5rem",
-											}}
-											label='Password'
-											variant='outlined'
-										/>
-										<span className='red-text'>
-											{errors.password}
-											{errors.passwordincorrect}
-										</span>
-									</div>
-									<div
-										style={{
-											display: "inline-block",
-											width: "100%",
-											fontSize: "12px",
-										}}>
-										<p
-											className='grey-text text-darken-1'
-											style={{ float: "left" }}>
-											<Link to='/forget-password'>
-												Forget Password?
-											</Link>
-										</p>
-										<p
-											className='grey-text text-darken-1'
-											style={{ float: "right" }}>
-											Don't have an account?{" "}
-											<Link to='/register'>Register</Link>
-										</p>
-									</div>
+				<div className='container' style={{
+					height: 940,
+					position: "relative",
+					// backgroundSize: "100%",
+					// backgroundPosition: "100%",
+					backgroundImage: "url(" + wed + ")",
+					backgroundRepeat: "no-repeat",
+				}}>
+					<Navbar customProps={this.state} />
+					<div
+						style={{
+							marginTop: "12%"
+						}}
+						className='row'>
+						<Container maxWidth='sm'>
+							<Card>
+								<CardContent style={{padding: "7%"}}>
 									<div
 										className='col s12'
 										style={{
-											paddingLeft: "11.250px",
 											display: "flex",
-											justifyContent: "center",
+											flexDirection: "column",
+											alignItems: "center",
+											marginBottom: "2rem",
 										}}>
-										<Button
-											type='submit'
-											variant='contained'
-											style={{
-												marginTop: "1rem",
-												fontFamily:
-													"Sailec-Bold,Helvetica,sans-serif",
-												fontWeight: "bold",
-												letterSpacing: "1px",
-												color: "#fff",
-												backgroundColor: "#22ba6a",
-											}}>
-											LogIn
-										</Button>
+										<Avatar>
+											<LockOutlinedIcon />
+										</Avatar>
+										<Typography component='h1' variant='h5'>
+											Sign in
+									</Typography>
 									</div>
-								</form>
-							</CardContent>
-						</Card>
-					</Container>
+									<form noValidate onSubmit={this.onSubmit}>
+										<div className='input-field col s12'>
+											<TextField
+												onChange={this.onChange}
+												value={this.state.email}
+												error={errors.email}
+												id='outlined-basic'
+												name='email'
+												type='email'
+												className={classnames("", {
+													invalid:
+														errors.email ||
+														errors.emailnotfound,
+												})}
+												style={{ width: "100%" }}
+												label='Email'
+												variant='outlined'
+											/>
+											<span className='red-text'>
+												{errors.email}
+												{errors.emailnotfound}
+											</span>
+										</div>
+										<div className='input-field col s12'>
+											<TextField
+												onChange={this.onChange}
+												value={this.state.password}
+												error={errors.password}
+												id='outlined-basic'
+												name='password'
+												type='password'
+												className={classnames("", {
+													invalid:
+														errors.password ||
+														errors.passwordincorrect,
+												})}
+												style={{
+													width: "100%",
+													marginTop: "0.5rem",
+												}}
+												label='Password'
+												variant='outlined'
+											/>
+											<span className='red-text'>
+												{errors.password}
+												{errors.passwordincorrect}
+											</span>
+										</div>
+										<div
+											style={{
+												display: "inline-block",
+												width: "100%",
+												fontSize: "12px",
+											}}>
+											<p
+												className='grey-text text-darken-1'
+												style={{ float: "left" }}>
+												<Link to='/forget-password'>
+													Forget Password?
+											</Link>
+											</p>
+											<p
+												className='grey-text text-darken-1'
+												style={{ float: "right" }}>
+												Don't have an account?{" "}
+												<Link to='/register'>Register</Link>
+											</p>
+										</div>
+										<div
+											className='col s12'
+											style={{
+												paddingLeft: "11.250px",
+												display: "flex",
+												justifyContent: "center",
+											}}>
+											<Button
+												type='submit'
+												variant='contained'
+												style={{
+													marginTop: "1rem",
+													fontFamily:
+														"Sailec-Bold,Helvetica,sans-serif",
+													fontWeight: "bold",
+													letterSpacing: "1px",
+													color: "#fff",
+													backgroundColor: "#22ba6a",
+												}}>
+												LogIn
+										</Button>
+										</div>
+									</form>
+								</CardContent>
+							</Card>
+						</Container>
+					</div>
 				</div>
-			</div>
 		);
 	}
 }
